de0cb2b24d5c51100a7003b341bf2e8d52ea94ad
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-02 20:05:06 -0300

    estilo basico

--- components/Principal/Principal.jsx
+++ components/Principal/Principal.jsx
@@ -1,27 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
+import "./Principal.css"
 
 export default function Principal()
 {
     console.log("owo")
     return(
         <div id="principalContainer">
-            <h1 id='principalNombre'>Ivanna Mohorcic</h1>
-            <h2 id='principalTitulo'>Desarrolladora Web - Full Stack</h2>
+            <div id="principalCardShell">
+                <div id="principalNameCont"><h1 id='principalNombre'>Ivanna Minerva Mohorcic</h1></div>
+                <div id="principalTituloCont"><h2 id='principalTitulo'>Desarrolladora Web - Full Stack</h2></div>
+                    <div className="principalShellExternos">
+                        <a target={"_blank"} href="https://linkedin.com/in/ivanna-mohorcic-94b96420a" className="principalLinksExternos">Linked-In</a>
+                    </div>
+        
+                    <div className="principalShellExternos">
+                        <a target={"_blank"} href="https://github.com/ezmohorcic" className="principalLinksExternos">GitHub</a>
+                    </div>
+                <div id='linksPrincipalCont'></div>
 
-            <div className="principalContLinksExternos">
-                <a target={"_blank"} href="https://linkedin.com/in/ivanna-mohorcic-94b96420a" className="principalLinksExternos">Linked-In</a>
+
+                {/* <div className="principalContLinksExternos">
+                    <Link></Link>
+                </div> */}
             </div>
 
-            <div className="principalContLinksExternos">
-                <a target={"_blank"} href="https://github.com/ezmohorcic" className="principalLinksExternos">GitHub</a>
-            </div>
-
-            {/* <div className="principalContLinksExternos">
-                <Link></Link>
-            </div> */}
         </div>
     )
 }
