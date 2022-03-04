555c62b4a9ad473578bf5ecc9662bffc8260dc66
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-04 16:15:57 -0300

    draft 4/3/22 16.15

--- components/Principal/Principal.jsx
+++ components/Principal/Principal.jsx
@@ -11,14 +11,16 @@ export default function Principal()
             <div id="principalCardShell">
                 <div id="principalNameCont"><h1 id='principalNombre'>Ivanna Minerva Mohorcic</h1></div>
                 <div id="principalTituloCont"><h2 id='principalTitulo'>Desarrolladora Web - Full Stack</h2></div>
+
+                <div id='linksPrincipalCont'>
                     <div className="principalShellExternos">
-                        <a target={"_blank"} href="https://linkedin.com/in/ivanna-mohorcic-94b96420a" className="principalLinksExternos">Linked-In</a>
-                    </div>
-        
-                    <div className="principalShellExternos">
-                        <a target={"_blank"} href="https://github.com/ezmohorcic" className="principalLinksExternos">GitHub</a>
-                    </div>
-                <div id='linksPrincipalCont'></div>
+                            <a target={"_blank"} href="https://linkedin.com/in/ivanna-mohorcic-94b96420a" className="principalLinksExternos">Linked-In</a>
+                        </div>
+
+                        <div className="principalShellExternos principalDerechaLink">
+                            <a target={"_blank"} href="https://github.com/ezmohorcic" className="principalLinksExternos">GitHub</a>
+                        </div>
+                </div>
 
 
                 {/* <div className="principalContLinksExternos">
