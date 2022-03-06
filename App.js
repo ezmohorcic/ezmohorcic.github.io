99d72ef59147e529b58f2f8c54f9c6a2bcb86543
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-05 21:33:52 -0300

    added scroll

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment,useEffect,useState } from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store.js'
 import {BrowserRouter,Routes,Route} from "react-router-dom";
@@ -7,11 +7,41 @@ import Projects from './components/Projects/Projects.jsx';
 
 import "./App.css"
 
+function handleScroll()
+{
+  const y_axis = window.scrollY
+  console.log("scroll",y_axis)
+}
+
 export default function App()
 {
+
+ const [scrollPosition, setPosition] = useState({ scrollX: 0, scrollY: 0 })
+  let scroll=0;
+  useEffect(() => {
+    function updatePosition() {
+        setPosition({ scrollX: window.scrollX, scrollY: window.scrollY })
+        console.log(window.scrollY,scroll)
+        if(scroll==0 && window.scrollY!=0) 
+        {
+          window.scrollTo(0,document.documentElement.clientHeight)
+          scroll=document.documentElement.clientHeight;
+        }
+        else{scroll=window.scrollY}
+        
+    }
+
+    window.addEventListener('scroll', updatePosition)
+    updatePosition()
+
+    return () => window.removeEventListener('scroll', updatePosition)
+   }, [])
+  //console.log(document.documentElement.clientHeight)
   return(
     <Provider store={store}>
-      <Principal/><Projects/>
+      <div >
+        <Principal /><Projects/>
+      </div>      
     </Provider>
   )
 }
--- components/Principal/Principal.jsx
+++ components/Principal/Principal.jsx
@@ -19,27 +19,27 @@ export default function Principal()
 {
     return(
         <div id="principalContainer" >
-            <motion.div variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} className='contDecoLineas'>
+            <motion.div viewport={{once:true}} variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} className='contDecoLineas'>
                 <div className='circuloDeco'></div>
                 <div className='LineaDeco'></div>
             </motion.div>
 
-            <motion.div variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} id="principalCardShell">
+            <motion.div viewport={{once:true}} variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} id="principalCardShell">
                 <div  id="principalNameCont">
-                    <motion.h1 variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} id='principalNombre'>Ivanna Minerva Mohorcic</motion.h1>
+                    <motion.h1 viewport={{once:true}} variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} id='principalNombre'>Ivanna Minerva Mohorcic</motion.h1>
                 </div>
-                <div id="principalTituloCont"><motion.h2 variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} id='principalTitulo'>Desarrolladora Web - Full Stack</motion.h2></div>
+                <div id="principalTituloCont"><motion.h2 viewport={{once:true}} variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} id='principalTitulo'>Desarrolladora Web - Full Stack</motion.h2></div>
                 <div id='linksPrincipalCont'>
-                    <motion.div variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} className="principalShellExternos">
+                    <motion.div viewport={{once:true}} variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} className="principalShellExternos">
                         <a  target={"_blank"} href="https://linkedin.com/in/ivanna-mohorcic-94b96420a" className="principalLinksExternos">Linked-In</a>
                     </motion.div>
-                    <motion.div variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} className="principalShellExternos principalDerechaLink">
+                    <motion.div viewport={{once:true}} variants={pVariants} initial={"offScreen"} whileInView={"onScreen"} className="principalShellExternos principalDerechaLink">
                         <a  target={"_blank"} href="https://github.com/ezmohorcic" className="principalLinksExternos">GitHub</a>
                     </motion.div>
                 </div>
             </motion.div>
 
-            <motion.div variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} className='contDecoLineas'>    
+            <motion.div viewport={{once:true}} variants={CShellVariants} initial={"offScreen"} whileInView={"onScreen"} className='contDecoLineas'>    
                 <div className='LineaDeco'></div>
                 <div className='circuloDeco'></div>
             </motion.div>
