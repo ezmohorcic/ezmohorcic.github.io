fad710e64c1e28ba97caacaf2cf5e95b11e4805f
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-07 13:09:39 -0300

    added first animation

--- components/Principal/BackgroundPrincipal.jsx
+++ components/Principal/BackgroundPrincipal.jsx
@@ -5,32 +5,23 @@ import "./BackgroundPrincipal.css";
 
 const hourLength=document.documentElement.clientWidth*(25/100)
 
-const animationHourLine=
-{
-    // offsetRotate: [0,90,180,270,360],
-    // translate: "25vw"
-    offsetPath: "circle(50% at 25% 25%)"
-}
+const animationHourLine=[{rotate: [0,90,180,270,360]},{rotate: [45,135,225,315,405]},{rotate: [90,180,270,360,450]},{rotate: [135,225,315,405,495]}]
 
 const transitionHourLine=
 {
-    duration:5,
+    duration:30,
     repeat: Infinity,
+    ease:[0.5,0.5,0.5,0.5]
 }
 
 export default function BackgroundPrincipal()
 {
     return(
         <div id='contBackgroundPrincipal'>
-            {/* <div className='HourLineCenter'></div> */}
-            <motion.div animate={animationHourLine} transition={transitionHourLine} id='deg0' className='HourLine'></motion.div>
-            <motion.div id='deg45' className='HourLine'></motion.div>
-            <motion.div id='deg90' className='HourLine'></motion.div>
-            <motion.div id='deg135' className='HourLine'></motion.div>
-            <motion.div id='deg180' className='HourLine'></motion.div>
-            <motion.div id='deg225' className='HourLine'></motion.div>
-            <motion.div id='deg270' className='HourLine'></motion.div>
-            <motion.div id='deg315' className='HourLine'></motion.div>
+            <motion.div animate={animationHourLine[0]} transition={transitionHourLine}  className='HourLine'></motion.div>
+            <motion.div animate={animationHourLine[1]} transition={transitionHourLine}  className='HourLine'></motion.div>
+            <motion.div animate={animationHourLine[2]} transition={transitionHourLine}  className='HourLine'></motion.div>
+            <motion.div animate={animationHourLine[3]} transition={transitionHourLine}  className='HourLine'></motion.div>
         </div>
     )
 }
