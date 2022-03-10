9114c8c79a7d030213c5e022ca0d37d34c5df407
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-03-10 03:05:39 -0300

    added provisional hover transition

--- components/Principal/BackgroundPrincipal.jsx
+++ components/Principal/BackgroundPrincipal.jsx
@@ -31,10 +31,21 @@ const transitionMinuteLine=
     ease:[0.5,0.5,0.5,0.5]
 }
 
+
+const testVariants=
+{
+    test0:{width:document.documentElement.clientWidth*(8/10),background:"linear-gradient(90deg, rgba(255,255,255,1) 10%, rgba(55,55,146,0) 10%, rgba(9,9,121,0) 90%, rgba(255,255,255,1) 90%)"},
+    test1:{width:document.documentElement.clientWidth*(5/10),background:"linear-gradient(90deg, rgba(217,175,55,1) 10%, rgba(55,55,146,0) 10%, rgba(9,9,121,0) 90%, rgba(217,175,55,1) 90%)"}
+}
+const transitionTest=
+{
+    duration:0.5
+}
+
 export default function BackgroundPrincipal()
 {
     const linkHover = useSelector(state=> state.pLinkHover);
-    const [cycle,setCycle]= useCycle("TrueAn","falseAn");
+    const [cycle,setCycle]= useState("TrueAn","falseAn");
 
     useEffect(()=>
     {   
@@ -42,12 +53,16 @@ export default function BackgroundPrincipal()
     },[linkHover]);
     return(
         <div id='contBackgroundPrincipal' >
-            <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[0]} transition={transitionHourLine}  className='HourLine'></motion.div>
+            {console.log(linkHover,cycle)}
+            {/* <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[0]} transition={transitionHourLine}  className='HourLine'></motion.div>
             <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[1]} transition={transitionHourLine}  className='HourLine'></motion.div>
             <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[2]} transition={transitionHourLine}  className='HourLine'></motion.div>
-            <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[3]} transition={transitionHourLine}  className='HourLine'></motion.div>
+            <motion.div  style={HourLineStyle} initial={"falseAn"} animate={animationHourLine[3]} transition={transitionHourLine}  className='HourLine'></motion.div> */}
 
-            {/* <motion.div variants={animationHourLine} initial={"Frot0"} animate={cycle=="TrueAn"? "Trot0" : "Frot0"} transition={transitionHourLine}  className='HourLine'></motion.div> */}
+            <motion.div variants={testVariants} animate={linkHover==1? "test1": "test0"}  transition={transitionTest} id="test0"  className='HourLineCenter'></motion.div>
+            <motion.div variants={testVariants} animate={linkHover==1? "test1": "test0"}  transition={transitionTest} id="test45"  className='HourLineCenter'></motion.div>
+            <motion.div variants={testVariants} animate={linkHover==1? "test1": "test0"}  transition={transitionTest} id="test90"  className='HourLineCenter'></motion.div>
+            <motion.div variants={testVariants} animate={linkHover==1? "test1": "test0"}  transition={transitionTest} id="test135"  className='HourLineCenter'></motion.div>
         
             <motion.div style={MinuteLineStyle} animate={animationMinuteLine[0]} transition={transitionMinuteLine}  className='MinuteLine'></motion.div>
             <motion.div style={MinuteLineStyle} animate={animationMinuteLine[1]} transition={transitionMinuteLine}  className='MinuteLine'></motion.div>
