6fa245b4d0a130fd58478a00499d648604dc132a
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-04-11 12:13:11 -0300

    draft 12.13

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import store from "./redux/store.js"
+import { createStore, applyMiddleware, compose } from "redux";
+// import store from "./redux/store.js"
 import ReactDOM from 'react-dom';
 import App from './app';
 import { Provider, useDispatch } from 'react-redux';
+
+const store = createStore(
+  rootReducer
+);
 
 ReactDOM.render(    
   <Provider store={store}><App/></Provider>,
