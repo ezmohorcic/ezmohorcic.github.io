b98bd8428bb2ddcacb3680759cf4f67113cda010
Ivanna Mohorcic <ezmohorcic@gmail.com>
2022-04-11 11:59:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import store from './redux/store.js'
 import ReactDOM from 'react-dom';
 import App from './app';
 import { Provider, useDispatch } from 'react-redux';
 
 ReactDOM.render(    
-  <Provider ><App/></Provider>,
+  <Provider store={store}><App/></Provider>,
       document.getElementById('root')
     );
